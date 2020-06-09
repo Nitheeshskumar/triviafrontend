552eb8ee889b8e19d99532e8ca9d3179c1a87e8a
Nitheesh.S.Kumar <nitheesh.sk@inapp.com>
2020-06-09 20:38:45 +0530

    message

---
+++ src/components/Quiz.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransitionGroup } from 'react-transition-group';
+import Question from '../components/Question';
+import QuestionCount from '../components/QuestionCount';
+import AnswerOption from '../components/AnswerOption';
+
+function Quiz(props) {
+  function renderAnswerOptions(key) {
+    return (
+      <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={props.answer}
+        questionId={props.questionId}
+        onAnswerSelected={props.onAnswerSelected}
+      />
+    );
+  }
+
+  return (
+    <CSSTransitionGroup
+      className="container"
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={500}
+      transitionAppear
+      transitionAppearTimeout={500}
+    >
+      <div key={props.questionId}>
+        <div className="questionCount">
+          Question <span>{props.questionId}</span> of <span>{props.questionTotal}</span>
+        </div>
+        <h2 className="question">{props.question}</h2>
+        <ul className="answerOptions">
+          {props.answerOptions.map(key=> <AnswerOption
+        key={key.content}
+        answerContent={key.content}
+        answerType={key.type}
+        answer={props.answer}
+        questionId={props.questionId}
+        onAnswerSelected={props.onAnswerSelected}
+      />
+      )}
+        </ul>
+      </div>
+    </CSSTransitionGroup>
+  );
+}
+
+Quiz.propTypes = {
+  answer: PropTypes.string.isRequired,
+  answerOptions: PropTypes.array.isRequired,
+  question: PropTypes.string.isRequired,
+  questionId: PropTypes.number.isRequired,
+  questionTotal: PropTypes.number.isRequired,
+  onAnswerSelected: PropTypes.func.isRequired
+};
+
+export default Quiz;
